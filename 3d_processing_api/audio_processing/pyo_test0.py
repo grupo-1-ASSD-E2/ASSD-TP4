2a7be072a988d998601c1bafb66cadb6edca5d00
ffarall <37149322+ffarall@users.noreply.github.com>
2020-05-03 16:20:00 -0300

    merging

--- 3d_processing_api/audio_processing/pyo_test0.py
+++
@@ -1,7 +0,0 @@
-import pyo
-
-s = pyo.Server().boot()
-s.start()
-snd = 'C:/Users/facun/OneDrive/Desktop/ITBA/6C ASSD/ASSD-TP4/Resources/Audio files/Multitracks Bohemian Rhapsody/03 - Borap03.mp3'
-sf = pyo.SfPlayer(snd, speed=[.999,1], loop=True, mul=.25).out()
-a = pyo.Convolve(sf, pyo.SndTable(SNDS_PATH+'/accord.aif'), size=512, mul=.2).out()
