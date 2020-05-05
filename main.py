34a06d2711818545a3c1fb8525c8f076526d03af
gonzadavidov <gdavidov@itba.edu.ar>
2020-05-04 22:44:10 -0300

    agregada ARI API

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 if __name__ == "__main__":
     HRIR_interpreter = HRIRsInterpreter()
-    IR = HRIR_interpreter.get_IR(90,0,1.47)
+    IR = HRIR_interpreter.get_IR(90.0,0.0,1.47)
     fig, ax = plt.subplots(figsize=(15, 9))
     ax.plot(IR[0,:], label="left", linewidth=0.5,  marker='o', markersize=1)
     ax.plot(IR[1,:], label="right", linewidth=0.5,  marker='o', markersize=1)
