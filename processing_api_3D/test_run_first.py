cd6b288119dd96f3fa6f0f0d3dcd4ee978017708
ffarall <37149322+ffarall@users.noreply.github.com>
2020-05-11 21:18:47 -0300

    minor change in test run first

--- processing_api_3D/test_run_first.py
+++ processing_api_3D/test_run_first.py
@@ -2,7 +2,7 @@ from audio_processing.convolutioner import Convolutioner
 
 br_tracks_paths = []
 for i in range(1,25):
-    br_tracks_paths.append('././Resources/Audio files/Multitracks Bohemian Rhapsody/{:02d} - Borap{:02d}.wav'.format(i, i))
+    br_tracks_paths.append('././Resources/Audio files/Multitracks Bohemian Rhapsody/{:02d} - Borap{:02d}.mp3'.format(i, i))
 
 conv = Convolutioner(input_files=br_tracks_paths)
 conv.save_input_array('processing_api_3D/tests output/Bohemian Rhapsody ndarray.npy')
